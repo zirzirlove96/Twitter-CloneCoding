d2c50daea8c2edd15859695fecccbbdc89a7fb95
김지영 <zirzirlove96@github.com>
2022-09-01 23:02:19 +0900

    로그인/회원가입

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,37 @@
 import Router from "components/Router";
-import React , {useState} from "react";
+import React , {useEffect, useState} from "react";
 import {authService} from "firebase_config";
+import {getAuth, onAuthStateChanged} from "firebase/auth";
 
 function App() {
   //useState는 App.js에서 사용
   //props로 전달
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   //authService.currentUser => 유저가 로그인 했는지에 대한 확인하는 firebase 메서드
+
+  //초기화
+  const [init, setInit] = useState(false);
+
+  //상태의 변화를 알아내기 위해 사용!
+  useEffect(()=> {
+    const auth = getAuth();
+    //firebase의 함수로 로그인 상태값을 알려준다.
+    onAuthStateChanged(auth,(user)=>
+    {
+      if(user)
+      {
+        setIsLoggedIn(true);
+      }else
+      {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  
   return (
     <>
-    <Router isLoggedIn={isLoggedIn}/>
+    {init ? <Router isLoggedIn={isLoggedIn}/> : "Initializing..."}
     <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
     </>
   );
--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -7,6 +7,7 @@ const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setNewAccount] = useState(true); //계정확인
+    const [error, setError] = useState(""); //에러메세지
 
     //onChange 이벤트가 있어야 변경이 가능함
     const onChange = (event) => {
@@ -21,6 +22,7 @@ const Auth = () => {
             setPassword(value);
         }
     }
+
     //authService는 promise 동작을 하므로 async, await 사용
     const onSubmit = async (event) => {
         event.preventDefault();
@@ -38,16 +40,26 @@ const Auth = () => {
             console.log(data);
         }catch(error)
         {
-            console.log(error);
+            console.log(error.message);
+            setError(error.message);
         }
 
     }
+
+    //값의 반대되는 값 리턴
+    //만든 이유 로그인과 회원가입을 왔다 갔다 하는 버튼 역할
+    const toggleAccount = () => setNewAccount((prev) => !prev); 
+
     return (<span>
         <form onSubmit={onSubmit}>
             <input type="email" name="email" placeholder="email" required value={email} onChange={onChange}/>
             <input type="password" name="password" placeholder="password" required value={password} onChange={onChange}/>
             <input type="submit" value={newAccount ? "Create Account" : "Log in"}/>
         </form>
+        <span onClick={toggleAccount}>
+            {newAccount ? "Sign in" : "Create Account"}
+        </span>
+        {error}
         <div>
             <button>Continue with Google</button>
         </div>
